b582efb5b6688f84d205528a6a2fe268793b8913
Artur Małecki <seeweer@gmail.com>
2015-02-10 22:26:48 +0000

    Improves countdown timer

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ var game = new Phaser.Game(300, 300, Phaser.CANVAS, "countdown", { create: creat
 function create() {
   timerEvent = this.time.events.loop(Phaser.Timer.SECOND, updateTimer);
   countDownText = this.add.text(0, 100, timer, { font: "65px Arial", fill: "#ff0044", align: "center" });
+
+  this.scale.pageAlignHorizontally = true;
+
   centerCountDownText();
 }
 
 function updateTimer() {
   timer -= 1;
   if(timer === 0) {
-    game.time.events.remove(timerEvent);
+    //To remove event:
+    //game.time.events.remove(timerEvent);
+    timer = 11;
     countDownText.setText("DONE");
   } else {
     countDownText.setText(timer);
